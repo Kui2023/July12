c92d9768fd535d71f0b11ff10bfa5303d063c654
Kui2023 <146767683+Kui2023@users.noreply.github.com>
2024-07-13 16:22:22 -0700

    Update app.py

--- app.py
+++ app.py
@@ -18,10 +18,10 @@ uploaded_file = st.file_uploader("Upload your input CSV file",
 if  uploaded_file is not None:
     
     data = pd.read_csv(uploaded_file)
-    #st.dataframe(data.head(num_rows))
+    
 else:
     st.warning("No data to display.") 
 num_rows = st.number_input('Enter the number of rows to display', 
                             min_value=0, max_value=30, value=5)
 st.header("Data Sample")
-
+st.dataframe(data.head(num_rows))
